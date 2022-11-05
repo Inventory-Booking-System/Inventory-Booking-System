9c0622a79bfd1f960226fadf2c3c01049b5cb30c
Ryan Coombes <ryan.coombes1@gmail.com>
2022-11-05 17:11:56 +0000

    Updates and tidy

--- app/Http/Livewire/Loan/Loans.php
+++ app/Http/Livewire/Loan/Loans.php
@@ -8,6 +8,8 @@ use App\Http\Livewire\DataTable\WithBulkActions;
 use App\Http\Livewire\DataTable\WithPerPagePagination;
 use App\Models\Loan;
 use App\Models\User;
+use App\Models\Asset;
+use Carbon\Carbon;
 
 class Loans extends Component
 {
@@ -28,6 +30,7 @@ class Loans extends Component
 
     public $counter = 0;
     public Loan $editing;
+    public $equipment_id;
 
     public $shoppingCart = [];
     public $shoppingCost = 0;
@@ -42,9 +45,10 @@ class Loans extends Component
         return [
             'editing.user_id' => 'required|integer',
             'editing.status_id' => 'required|string|in:0,1',
-            'editing.start_date_time' => 'required|date|before:end_date|nullable',
-            'editing.end_date_time' => 'required|date|after:start_date|nullable',
+            'editing.start_date_time' => 'required|date|before:editing.end_date_time|nullable',
+            'editing.end_date_time' => 'required|date|after:editing.start_date_time|nullable',
             'editing.details' => 'nullable|string',
+            'equipment_id' => 'nullable|numeric|exists:assets,id',
         ];
     }
 
@@ -67,6 +71,8 @@ class Loans extends Component
     public function makeBlankLoan()
     {
         $this->editing = Loan::make();
+        $shoppingCart = [];
+        $equipment_id = null;
     }
 
     public function deleteSelected()
@@ -105,7 +111,21 @@ class Loans extends Component
     {
         $this->validate();
 
+        $this->editing->start_date_time = carbon::parse($this->editing->start_date_time);
+        $this->editing->end_date_time = carbon::parse($this->editing->end_date_time);
+
         $this->editing->save();
+
+        $loan = Loan::find($this->editing->id);
+
+        //Add equipment issues into equipment_issue_incidents
+        $ids = [];
+        //dd($this->shoppingCart);
+        foreach($this->shoppingCart as $key => $item){
+            array_push($ids, ['loan_id' => $this->editing->id, 'asset_id' => $key, 'returned' => $item['returned']]);
+        }
+
+        $loan->assets()->sync($ids);
 
         $this->emit('hideModal', 'edit');
     }
@@ -142,5 +162,72 @@ class Loans extends Component
         return view('livewire.loan.loans', [
             'loans' => $this->rows,
         ]);
+    }
+
+    /**
+     * Fetches a list of equipment avaliable for the current selected input values.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function getBookableEquipment()
+    {
+        $validatedDate =[
+            'start_date_time' => carbon::parse($this->editing->start_date_time),
+            'end_date_time' => carbon::parse($this->editing->end_date_time),
+            'id' =>  $this->loanId ?? -1
+        ];
+
+        $this->avaliableEquipment = Asset::with('loans')
+            ->where(function($query) use($validatedDate){
+                $query->whereNotIn('assets.id', function($query) use($validatedDate){
+                    $query->select('asset_loan.asset_id')
+                            ->from('loans')
+                            ->join('asset_loan','loans.id','asset_loan.loan_id')
+                            ->whereRaw('`assets`.`id` = `asset_loan`.`asset_id`')
+                            ->where('loans.id', '!=', $validatedDate['id'])
+                            ->where(function($query2) use($validatedDate){
+                                $query2->where('loans.start_date_time', '>=', $validatedDate['start_date_time'])
+                                        ->where('loans.start_date_time', '<=', $validatedDate['end_date_time'])
+                                        ->where('loans.id', '!=', $validatedDate['id']);
+                            })->orWhere(function($query2) use($validatedDate){
+                                $query2->where('loans.end_date_time', '>=', $validatedDate['start_date_time'])
+                                    ->where('loans.end_date_time', '<=', $validatedDate['end_date_time'])
+                                    ->where('loans.id', '!=', $validatedDate['id']);
+                            })
+                            ->where('asset_loan.returned','=',0);
+                })
+                ->orWhereDoesntHave('loans');
+            })
+            ->get();
+
+        $this->emit('refresh');
+    }
+
+    public function updatedEquipmentId($id)
+    {
+        $item = Asset::find($id);
+
+        $this->shoppingCart[$item->id] = [];
+        $this->shoppingCart[$item->id]['title'] = $item->name;
+        $this->shoppingCart[$item->id]['asset_id'] = $item->tag;
+        $this->shoppingCart[$item->id]['returned'] = 0;
+
+        //dd($this->shoppingCart);
+    }
+
+    public function removeItem($id)
+    {
+        if($this->shoppingCart[$id]['quantity'] == 1){
+            unset($this->shoppingCart[$id]);
+        }elseif($this->shoppingCart[$id]['quantity'] > 1){
+            $this->shoppingCart[$id]['quantity'] -= 1;
+        }
+    }
+
+    public function updatedEditingEndDateTime()
+    {
+        $this->getBookableEquipment();
+        $this->iteration ++;
     }
 }
