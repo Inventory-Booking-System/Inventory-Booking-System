d1572cb7c0cf60e30e6c11c8edad6646d94cbb57
Angus McDougall <mcdougall.angus@ashcombe.surrey.sch.uk>
2023-01-05 12:18:12 +0000

    Fix modal stealing focus from select search boxes

--- resources/views/components/modal/dialog.blade.php
+++ resources/views/components/modal/dialog.blade.php
@@ -1,7 +1,7 @@
 @props(['id' => null, 'maxWidth' => null, 'type' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div wire:ignore.self class="modal fade" id="{{ $type }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div wire:ignore.self class="modal fade" id="{{ $type }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-focus="false">
         <div {{ $attributes->merge(['class' => 'modal-dialog']) }}>
           <div class="modal-content">
             <div class="modal-header">
