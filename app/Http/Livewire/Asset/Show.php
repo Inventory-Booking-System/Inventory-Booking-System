a2a23fbe6f17ff24c734cf7a4e0ae11693ee55f2
Anjasomc <anjasomc@me.com>
2023-02-27 12:35:24 +0000

    Asset details table filtering

--- app/Http/Livewire/Asset/Show.php
+++ app/Http/Livewire/Asset/Show.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\DataTable\WithPerPagePagination;
 use Carbon\Carbon;
 use App\Models\Asset;
 use App\Models\Loan;
+use App\Helpers\SQL;
 
 class Show extends Component
 {
@@ -22,9 +23,13 @@ class Show extends Component
 
     public $filters = [
         'search' => '',
-        'name' => null,
-        'tag' => null,
-        'description' => null,
+        'id' => null,
+        'user_id' => null,
+        'status_id' => null,
+        'start_date_time' => null,
+        'end_date_time' => null,
+        'details' => null,
+        'assets' => null,
     ];
 
     protected $queryString = [];
@@ -49,6 +54,96 @@ class Show extends Component
         }
     }
 
+    private function searchById($query, $search, $orWhere = false) {
+        $search = SQL::escapeLikeString($search);
+        // Handle searching by ID if the user has entered a leading #
+        if ($orWhere) {
+            $query->where('loans.id', 'like', '%'.str_replace('#', '', $search).'%');
+        } else {
+            $query->where('loans.id', 'like', '%'.str_replace('#', '', $search).'%');
+        }
+    }
+
+    private function searchByUser($query, $search, $orWhere = false) {
+        $search = SQL::escapeLikeString($search);
+        if ($orWhere) {
+            $query->orWhere(DB::raw("CONCAT(users.forename, ' ', users.surname)"), 'like', '%'.$search.'%');
+        } else {
+            $query->where(DB::raw("CONCAT(users.forename, ' ', users.surname)"), 'like', '%'.$search.'%');
+        }
+    }
+
+    private function searchByStatus($query, $search, $orWhere = false) {
+        $search = SQL::escapeLikeString($search);
+        foreach (Loan::getStatusIds() as $id => $status) {
+            if (str_contains(strtolower($status), strtolower($search))) {
+                if ($orWhere) {
+                    $query->orWhere('loans.status_id', $id);
+                } else {
+                    $query->where('loans.status_id', $id);
+                }
+            }
+        }
+    }
+
+    private function searchByStartDate($query, $search, $orWhere = false) {
+        $search = SQL::escapeLikeString($search);
+        try {
+            $dateTimeString = Carbon::parse($search);
+            $dateString = explode(' ', $dateTimeString)[0];
+            $timeString = explode(' ', $dateTimeString)[1];
+            if ($orWhere) {
+                $query->orWhere('loans.start_date_time', 'like', '%'.$dateString.'%');
+                $query->orWhere('loans.start_date_time', 'like', '%'.$timeString.'%');
+            } else {
+                $query->where('loans.start_date_time', 'like', '%'.$dateString.'%');
+                $query->orWhere('loans.start_date_time', 'like', '%'.$timeString.'%');
+            }
+        } catch(\Throwable $e) {
+            // Search string is not a date
+        }
+    }
+
+    private function searchByEndDate($query, $search, $orWhere = false) {
+        $search = SQL::escapeLikeString($search);
+        try {
+            $dateTimeString = Carbon::parse($search);
+            $dateString = explode(' ', $dateTimeString)[0];
+            $timeString = explode(' ', $dateTimeString)[1];
+            if ($orWhere) {
+                $query->orWhere('loans.end_date_time', 'like', '%'.$dateString.'%');
+                $query->orWhere('loans.end_date_time', 'like', '%'.$timeString.'%');
+            } else {                
+                $query->where('loans.end_date_time', 'like', '%'.$dateString.'%');
+                $query->orWhere('loans.end_date_time', 'like', '%'.$timeString.'%');
+            }
+        } catch(\Throwable $e) {
+            // Search string is not a date
+        }
+    }
+
+    private function searchByDetails($query, $search, $orWhere = false) {
+        $search = SQL::escapeLikeString($search);
+        if ($orWhere) {
+            $query->orWhere('loans.details', 'like', '%'.$search.'%');
+        } else {
+            $query->where('loans.details', 'like', '%'.$search.'%');
+        }
+    }
+
+    private function searchByAssets($query, $search, $orWhere = false) {
+        $search = SQL::escapeLikeString($search);
+        if ($orWhere) {
+            $query->orWhereHas('assets', function ($query) use ($search) {
+                $query->where(DB::raw("CONCAT(name, ' ', '(', tag, ')')"), 'like', '%'.$search.'%');
+            });
+        } else {
+            $query->whereHas('assets', function ($query) use ($search) {
+                $query->where(DB::raw("CONCAT(name, ' ', '(', tag, ')')"), 'like', '%'.$search.'%');
+            });
+        }
+    }
+
     public function getRowsQueryProperty()
     {
         $asset = $this->asset;
@@ -59,61 +154,22 @@ class Show extends Component
             ->whereHas('assets', function($query) use($asset){
                 $query->where('tag', '=', $asset->tag);
             })
-            ->where(function($query) { // Search
-                // Loan ID
-                $query->when($this->filters['search'], fn($query, $search) =>
-                    // Handle searching by ID if the user has entered a leading #
-                    $query->orWhere('loans.id', 'like', '%'.str_replace('#', '', $search).'%'))
-
-                // User
-                ->when($this->filters['search'], fn($query, $search) =>
-                    $query->orWhere(DB::raw("CONCAT(users.forename, ' ', users.surname)"), 'like', '%'.$search.'%'))
-
-                // Status
-                ->when($this->filters['search'], function($query, $search) {
-                    foreach (Loan::getStatusIds() as $id => $status) {
-                        if (str_contains(strtolower($status), strtolower($search))) {
-                            $query->orWhere('loans.status_id', $id);
-                        }
-                    }
-                })
-
-                // Start Date
-                ->when($this->filters['search'], function($query, $search) {
-                    try {
-                        $dateTimeString = Carbon::parse($search);
-                        $dateString = explode(' ', $dateTimeString)[0];
-                        $timeString = explode(' ', $dateTimeString)[1];
-                        $query->orWhere('loans.start_date_time', 'like', '%'.$dateString.'%');
-                        $query->orWhere('loans.start_date_time', 'like', '%'.$timeString.'%');
-                    } catch(\Throwable $e) {
-                        // Search string is not a date
-                    }
-                })
-
-                // End Date
-                ->when($this->filters['search'], function($query, $search) {
-                    try {
-                        $dateTimeString = Carbon::parse($search);
-                        $dateString = explode(' ', $dateTimeString)[0];
-                        $timeString = explode(' ', $dateTimeString)[1];
-                        $query->orWhere('loans.end_date_time', 'like', '%'.$dateString.'%');
-                        $query->orWhere('loans.end_date_time', 'like', '%'.$timeString.'%');
-                    } catch(\Throwable $e) {
-                        // Search string is not a date
-                    }
-                })
-
-                // Details
-                ->when($this->filters['search'], fn($query, $search) =>
-                      $query->orWhere('loans.details', 'like', '%'.$search.'%'))
-
-                // Assets
-                ->when($this->filters['search'], fn($query, $search) => 
-                    $query->orWhereHas('assets', function ($query) use ($search) {
-                        $query->where(DB::raw("CONCAT(name, ' ', '(', tag, ')')"), 'like', '%'.$search.'%');
-                    }));
-            });
+            ->when($this->filters['id'], fn($query, $search) => $this->searchById($query, $search))
+            ->when($this->filters['user_id'], fn($query, $search) => $this->searchByUser($query, $search))
+            ->when($this->filters['status_id'], fn($query, $search) => $this->searchByStatus($query, $search))
+            ->when($this->filters['start_date_time'], fn($query, $search) => $this->searchByStartDate($query, $search))
+            ->when($this->filters['end_date_time'], fn($query, $search) => $this->searchByEndDate($query, $search))
+            ->when($this->filters['details'], fn($query, $search) => $this->searchByDetails($query, $search))
+            ->when($this->filters['assets'], fn($query, $search) => $this->searchByAssets($query, $search))
+            ->when($this->filters['search'], fn($query, $search) => $query->where(function($query) use ($search) {
+                $this->searchById($query, $search);
+                $this->searchByUser($query, $search, true);
+                $this->searchByStatus($query, $search, true);
+                $this->searchByStartDate($query, $search, true);
+                $this->searchByEndDate($query, $search, true);
+                $this->searchByDetails($query, $search, true);
+                $this->searchByAssets($query, $search, true);
+            }));
 
         return $this->applySorting($query);
     }
