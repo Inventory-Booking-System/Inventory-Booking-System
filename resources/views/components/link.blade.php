a9b00d07a2fca9af863ce7778e92d527712551e1
Anjasomc <anjasomc@me.com>
2023-03-14 11:25:21 +0000

    Add lineThrough prop to link

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -2,6 +2,13 @@
     'route' => null,
     'id' => null,
     'value' => null,
+    'lineThrough' => false
 ])
 
-<a href="/{{ $route }}/{{ $id }}" {{ $attributes }} >{{ $value }}</a>
+<a
+    href="/{{ $route }}/{{ $id }}"
+    @if($lineThrough) {{ $attributes->merge(['class' => 'line-through text-secondary']) }} @endif
+    {{ $attributes }}
+>
+    {{ $value }}
+</a>
