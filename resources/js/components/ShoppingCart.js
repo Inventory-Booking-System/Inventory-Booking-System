0d0336ce1181a4da597fab989f84d84fce7d5c53
Anjasomc <anjasomc@me.com>
2023-03-30 12:42:11 +0100

    Improved shopping cart styling

--- resources/js/components/ShoppingCart.js
+++ resources/js/components/ShoppingCart.js
@@ -1,39 +1,59 @@
 // eslint-disable-next-line no-unused-vars
 import { h } from 'preact';
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 
-function ItemCard({ index, name, tag, returned, onRemove, onReturn, action }) {
+function ItemCard({ index, name, quantity, tag, cost, returned, onRemove, onReturn, action }) {
 
     const remove = useCallback(() => onRemove(index), [index, onRemove]);
     const bookIn = useCallback(() => onReturn(index), [index, onReturn]);
 
     return (
         <Card className={returned ? 'bg-success' : ''}>
-            <Card.Body>
-                <Row>
-                    <Col>{name}</Col>
-                    <Col sm={2}>{tag}</Col>
-                    <Col sm={3}>
-                        {action !== 'Create' && <Button
-                            variant="light"
-                            onClick={bookIn}
-                        >
-                            <i className="fa-sharp fa-solid fa-circle-check"></i>
-                        </Button>}
-                        <Button
-                            variant="light"
-                            style="color: #cecece;"
-                            onClick={remove}
-                        >
-                            <i className="fas fa-trash-alt"></i>
-                        </Button>
-                    </Col>
-                </Row>
+            <Card.Body className="px-3 py-2 my-0">
+                <div className="d-flex justify-content-between">
+                    <div className="d-flex flex-row align-items-center">
+                        <div>
+                            <h5 className="mb-0">{name}</h5>
+                        </div>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center">
+
+                        {quantity && <div style="width: 50px;">
+                            <h5 className="fw-normal mb-0">x{quantity}</h5>
+                        </div>}
+
+                        {tag && <div style="width: 50px;">
+                            <h5 className="fw-normal mb-0">{tag}</h5>
+                        </div>}
+
+                        {cost && <div style="width: 80px;">
+                            <h5 className="mb-0">£{cost}</h5>
+                        </div>}
+
+                        <ButtonGroup>
+                            {action !== 'Create' && <Button
+                                variant={returned ? 'success' : 'light'}
+                                onClick={bookIn}
+                            >
+                                <i
+                                    className="fa-sharp fa-solid fa-circle-check"
+                                    style={returned ? 'color: #fff' : ''}
+                                />
+                            </Button>}
+                            <Button
+                                variant={returned ? 'success' : 'light'}
+                                onClick={remove}
+                            >
+                                <i className="fas fa-trash-alt"></i>
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+                </div>
             </Card.Body>
         </Card>
     );
@@ -65,9 +85,23 @@ export default function ShoppingCart({ action, assets, onChange }) {
         onChange(updatedShoppingCart);
     }, [assets, onChange]);
 
+    const totalCost = useMemo(() => {
+        let cost = 0;
+        for (var i = 0; i < assets.length; i++) {
+            cost += assets[i].cost;
+        }
+        return cost;
+    }, [assets]);
+
     return (
         <Card>
             <Card.Body>
+                {totalCost > 0 && <>
+                    <h5 className="mb-0 text-right text-body font-weight-bold">
+                        Total Cost <span className="font-weight-normal">{totalCost}</span>
+                    </h5>
+                    <hr />
+                </>}
                 {assets.map((asset, index) =>
                     <ItemCard
                         key={index}
